36e0ecc16ff7c7dd9bdc8d1ccca8510e1d230e74
gautamkmahato <gautamkmahato@gmail.com>
2020-12-26 13:42:45 +0530

    search value added

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.get('/', async (req, res) => {
 
-    const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.API_KEY}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=in&sortBy=popularity&apiKey=${process.env.API_KEY}`;
 
     try{
         await fetch(url)
@@ -45,6 +45,75 @@ app.get('/', async (req, res) => {
     }
     
 });
+
+app.get('/search', (req, res) => {
+    res.render('news');
+})
+
+app.post('/search', async (req, res) => {
+    const value = req.body.news;
+
+    const url = `https://newsapi.org/v2/everything?q=${value}&sortBy=popularity&apiKey=14f4539e5d534dc3814b1d7230e51a16`;
+
+    try{
+        await fetch(url)
+        .then((data) => data.json())
+        .then((data) => {
+            // res.render('home', {
+            //     data: data
+            //     // source: data.articles[0].source,
+            //     // author: data.articles[0].author,
+            //     // title: data.articles[0].title,
+            //     // description: data.articles[0].description,
+            //     // url: data.articles[0].url,
+            //     // image: data.articles[0].urlToImage,
+            //     // publishedAt: data.articles[0].publishedAt,
+            //     // content: data.articles[0].content
+            // })
+            res.send(data)
+            console.log(data);
+        });
+    }
+    catch(err){
+        console.log(err);
+    }
+
+})
+
+
+// app.get('/news', async (req, res) => {
+//     // var url = 'http://newsapi.org/v2/everything?' +
+//     //         'q=bitcoin&' +
+//     //         'from=2020-12-26&' +
+//     //         'sortBy=popularity&' +
+//     //         'apiKey=14f4539e5d534dc3814b1d7230e51a16';
+
+//     const url = 'https://newsapi.org/v2/everything?q=bitcoin&sortBy=popularity&apiKey=14f4539e5d534dc3814b1d7230e51a16';
+  
+    
+//     try{
+//         await fetch(url)
+//         .then((data) => data.json())
+//         .then((data) => {
+//             // res.render('home', {
+//             //     data: data
+//             //     // source: data.articles[0].source,
+//             //     // author: data.articles[0].author,
+//             //     // title: data.articles[0].title,
+//             //     // description: data.articles[0].description,
+//             //     // url: data.articles[0].url,
+//             //     // image: data.articles[0].urlToImage,
+//             //     // publishedAt: data.articles[0].publishedAt,
+//             //     // content: data.articles[0].content
+//             // })
+//             res.send(data)
+//             console.log(data);
+//         });
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// })
 
 
 app.listen('3000', function(){
